c90efc210eb7f8e1471ab9e38d7f1ebe3a6a9279
Guillaume Morin <gmorin@sysif.net>
2017-07-02 17:53:22 +0200

    use struct instead of int*

--- marcoyolo.h
+++ marcoyolo.h
@@ -4,6 +4,8 @@
 #define _XOPEN_SOURCE 500
 #endif /* __STDC_VERSION__ */
 
+#define MAP_SIZE_X 60
+#define MAP_SIZE_Y 20
 #define T_PAUSE 100000000L
 
 #include <stdio.h>
@@ -12,10 +14,15 @@
 #include <string.h>
 #include <time.h>
 
-int initialize (int* matrix, int s_rows, int s_cols);
-int destroy(int* matrix);
-int display(int* matrix, int s_rows, int s_cols);
+typedef struct s_map {
+  int ground[MAP_SIZE_X][MAP_SIZE_Y];
+  int color[MAP_SIZE_X][MAP_SIZE_Y];
+  int next_color;
+};
 
-int browse(int* matrix, int s_rows, int s_cols);
-int spread(int* matrix, int s_rows, int s_cols, int source_x, int source_y); 
+int init(struct s_map *map);
+int display(struct s_map *map);
+int browse(struct s_map *map);
+int nextcolor(struct s_map *map);
+int spread(struct s_map *map,int x, int y, int ground, int color);
 
